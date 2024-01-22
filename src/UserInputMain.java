08b31efb7b7ad9af165bf160d92db62a88abb465
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-01-22 12:03:45 +0100

    Interactive User Input

--- src/UserInputMain.java
+++ src/UserInputMain.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class UserInputMain {
     public static void main(String[] args) {
         //Scanner is a class provided by Java to interact with Inputs
+        System.out.println("Hello, what is your name?");
         Scanner sc = new Scanner(System.in);
-        String firstStr = sc.nextLine();
-        System.out.println("You entered : " + firstStr);
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
 
+        System.out.println("How old are you?");
         int age = sc.nextInt();
-        System.out.println("Your age : " + age);
+        System.out.println("I am " + age + " years old");
     }
 }
